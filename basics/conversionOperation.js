87c0cac61e23a19197f86e788afdcc5bae0eaef7
Arka Mandal <123437008+ArkaMandal16@users.noreply.github.com>
2024-04-27 19:22:00 +0000

    learned a little bit conversion

--- basics/conversionOperation.js
+++ basics/conversionOperation.js
@@ -1,3 +1,18 @@
-let score = 50
+let score = "50"
 
 console.log(typeof score);
+
+let value_in_number = Number(score)
+console.log(value_in_number);
+console.log(typeof value_in_number);
+
+let bool_value = true
+let is = 1
+console.log(typeof bool_value);
+console.log(typeof is);
+
+value_in_number = Number(bool_value)
+bool_in_number = Boolean(is)
+
+console.log(value_in_number)
+console.log(bool_in_number)
