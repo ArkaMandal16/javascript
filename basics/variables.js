065a1cb03eb6918769262efc2ac84f19791d03c2
Arka Mandal <123437008+ArkaMandal16@users.noreply.github.com>
2024-04-27 17:13:21 +0000

    added datatypes file

--- basics/variables.js
+++ basics/variables.js
@@ -3,3 +3,11 @@ let account_email = "mandalarka16@gmail.com"
 var account_password = "123345"
 
 console.log(account_id);
+
+console.table([account_id, account_email, account_password])
+
+/*
+var not good to use because of function scope
+*/
+let account_state;
+console.table([account_id, account_email, account_password, account_state])
